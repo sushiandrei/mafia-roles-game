ddbcb9aba848c6664d4b97411fabb14fa45fef76
sushiandrei <132266373+sushiandrei@users.noreply.github.com>
2023-05-02 13:36:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   // Global variables
   let numPlayers;
   let numKillers;
   let hasPriest;
   let playerName;
-  let gameCode;
 
   // DOM elements
   const joinGameForm = document.getElementById('join-game-form');
@@ -19,13 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameCodeDisplay = document.getElementById('game-code-display');
   const gameRoleDisplay = document.getElementById('game-role-display');
   const errorMessage = document.getElementById('error-message');
-  const startGameButton = document.getElementById('start-game-button');
-  const playerListDisplay = document.getElementById('player-list-display');
-  const statusDisplay = document.getElementById('status-display');
 
   // Functions
   function createGame() {
-    console.log('Create game button clicked');
     numPlayers = parseInt(numPlayersInput.value);
     numKillers = parseInt(numKillersInput.value);
     hasPriest = hasPriestInput.checked;
@@ -37,22 +33,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Generate game code
-    gameCode = Math.floor(Math.random() * 10000);
+    const gameCode = Math.floor(Math.random() * 10000);
     gameCodeDisplay.textContent = gameCode;
 
     // Hide create game form and show game code
     createGameForm.style.display = 'none';
     gameCodeDisplay.style.display = 'block';
 
-    // Show start game button
-    startGameButton.style.display = 'block';
-
     // TODO: Save game data to database
   }
 
   function joinGame() {
-    console.log('Join game button clicked');
-    gameCode = gameCodeInput.value;
+    const gameCode = gameCodeInput.value;
 
     // TODO: Fetch game data from database
 
@@ -65,21 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // TODO: Generate and display game role based on game data
   }
 
-  function startGame() {
-    // TODO: Update game data in database to indicate game has started
-
-    // Hide game code and start game button
-    gameCodeDisplay.style.display = 'none';
-    startGameButton.style.display = 'none';
-
-    // Show player list display
-    playerListDisplay.style.display = 'block';
-
-    // TODO: Display list of players who have joined the game
-  }
-
   // Event listeners
   createGameButton.addEventListener('click', createGame);
   joinGameButton.addEventListener('click', joinGame);
-  startGameButton.addEventListener('click', startGame);
+
 });
