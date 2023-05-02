fcee1353260d2a0f9affd2c98b842061439964ff
sushiandrei <132266373+sushiandrei@users.noreply.github.com>
2023-05-02 12:52:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let numPlayers;
 let numKillers;
 let hasPriest;
 let playerName;
+let gameData;
 
 // DOM elements
 const joinGameForm = document.getElementById('join-game-form');
@@ -17,6 +18,11 @@ const createGameButton = document.getElementById('create-game-button');
 const gameCodeDisplay = document.getElementById('game-code-display');
 const gameRoleDisplay = document.getElementById('game-role-display');
 const errorMessage = document.getElementById('error-message');
+const gameStartButton = document.getElementById('game-start-button');
+const playerList = document.getElementById('player-list');
+const storytellerPage = document.getElementById('storyteller-page');
+const playersPage = document.getElementById('players-page');
+const gameStatusLabel = document.getElementById('game-status-label');
 
 // Functions
 function createGame() {
@@ -39,24 +45,48 @@ function createGame() {
   gameCodeDisplay.style.display = 'block';
 
   // TODO: Save game data to database
+  gameData = {
+    gameCode: gameCode,
+    numPlayers: numPlayers,
+    numKillers: numKillers,
+    hasPriest: hasPriest,
+    players: []
+  };
 }
 
 function joinGame() {
   const gameCode = gameCodeInput.value;
 
   // TODO: Fetch game data from database
+  gameData = {
+    gameCode: gameCode,
+    numPlayers: 8,
+    numKillers: 2,
+    hasPriest: true,
+    players: [
+      { name: 'Player 1', role: 'Innocent' },
+      { name: 'Player 2', role: 'Killer' },
+      { name: 'Player 3', role: 'Innocent' },
+      { name: 'Player 4', role: 'Killer' },
+      { name: 'Player 5', role: 'Innocent' },
+      { name: 'Player 6', role: 'Innocent' },
+      { name: 'Player 7', role: 'Innocent' },
+      { name: 'Player 8', role: 'Innocent' }
+    ]
+  };
 
   // TODO: Validate game data and show error message if invalid
+  if (!gameData) {
+    errorMessage.textContent = 'Invalid game code.';
+    return;
+  }
 
   // Hide join game form and show game role
   joinGameForm.style.display = 'none';
   gameRoleDisplay.style.display = 'block';
 
   // TODO: Generate and display game role based on game data
-}
-
-// Event listeners
-document.addEventListener('DOMContentLoaded', function() {
-  createGameButton.addEventListener('click', createGame);
-  joinGameButton.addEventListener('click', joinGame);
-});
+  const player = gameData.players.find(player => player.name === playerNameInput.value);
+  if (player) {
+    gameStatusLabel.textContent = 'The game has started.';
+    gameStartButton.style.display
